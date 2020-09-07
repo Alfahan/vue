d19c47bd8d0799853e79d44c301e76c7b3074072
Alfahan <ali.farhan160@gmail.com>
2020-09-07 10:36:54 +0700

    Vue

---
+++ src/components/Products.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>IDShop</h1>
+        <navbar :cart="cart" :cartQty="cartQty" :cartTotal="cartTotal"></navbar>
+        <price-slider :sliderStatus=" sliderStatus"></price-slider>
+        <product-list :maximum="maximum" :products="products"></product-list>
+    </div>
+</template>
+
+<script>
+import ProductList from './ProductList.vue';
+import PriceSlider from './PriceSlider.vue';
+import Navbar from './Navbar.vue';
+
+export default {
+    name: "products",
+    props: [
+        "products", 
+        "maximum", 
+        "cart", 
+        "cartQty", 
+        "cartTotal", 
+        "sliderStatus", 
+        "sliderState"
+    ],
+    components: {
+        Navbar,
+        PriceSlider,
+        ProductList,
+    }
+}
+</script>
